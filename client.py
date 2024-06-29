4689a1ff9e1f58c724a459adeca69fc6052a7aa0
Muhammad Sajid <56728993+sajidcodess@users.noreply.github.com>
2024-06-29 13:52:48 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,31 @@
 import socket
 import threading
 
-def receive_message(client_socket):
+def receive_message(client_socket, user_id):
     while True:
         try:
             response = client_socket.recv(1024).decode('utf-8')
-            print(response)
-        except:
-            print("connection lost")
+            if not response:
+                print("Connection closed by the server")
+                break
+            sender_id, message = response.split(": ", 1)
+            if sender_id == user_id:
+                print(f"ME: {message}")
+            else:
+                print(f"{sender_id}: {message}")
+        except Exception as e:
+            print(f"Error receiving message: {e}")
             client_socket.close()
             break
 
-def send_message(client_socket):
+def send_message(client_socket, user_id):
     while True:
         try:
-            reply = input("ME: ")
-            client_socket.send(reply.encode('utf-8'))
+            message = input("Me: ")
+            if message.strip().lower() == "exit":
+                client_socket.close()
+                break
+            client_socket.send(f"{user_id}: {message}".encode('utf-8'))
         except Exception as e:
             raise e
             
@@ -26,9 +36,12 @@ def start_client():
     server_port = 8000
     client_socket.connect((server_ip, server_port))
     print("-------connected to the server----------\n")
+    print("-------Welcome to terminal chatting---------\n")
+    user_id = input("To begin the chatting, please assign yourself any name: ")
+    print('------happy chatting, to exit the chat simply send the word "exit" or ctrl+c"--------\n')
 
-    threading.Thread(target=receive_message, args=(client_socket,)).start()
-    threading.Thread(target=send_message, args=(client_socket,)).start()
+    threading.Thread(target=receive_message, args=(client_socket, user_id)).start()
+    threading.Thread(target=send_message, args=(client_socket, user_id)).start()
 
 
 
