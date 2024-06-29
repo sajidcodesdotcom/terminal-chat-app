73f418e9c6ca4a5b4c2d07971e02d07cd611161f
Muhammad Sajid <56728993+sajidcodess@users.noreply.github.com>
2024-06-29 13:52:21 -0700

    update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import concurrent.futures
 
 # list to keep track of connected clients
 clients = []
@@ -8,14 +9,15 @@ def handle_client(client_socket):
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
+            if message == "exist":
+                remove_client(client_socket)
+                break
             if message:
                 broadcast_message(message, client_socket)
             else:
-                remove_client(client_socket)
-                break
+                print("Somthing went wrong while broadcasting the message")
         except Exception as e:
             raise e
-    client_socket.close()
 
 def broadcast_message(message, client_socket):
     for client in clients:
@@ -26,21 +28,28 @@ def broadcast_message(message, client_socket):
 def remove_client(client_socket):
     if client_socket in clients:
         clients.remove(client_socket)
+        client_socket.close()
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_ip = "0.0.0.0"
     port = 8000
     server.bind((server_ip, port))
-    server.listen(5) # listen for 5 connections (more than 10 clients are not allowed to connect simultaneously.
+    server.listen(10) # listen for 5 connections (more than 10 clients are not allowed to connect simultaneously.
     print(f"listening on {server_ip}:{port}")
 
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=10) 
     while True:
-        client_socket, client_address = server.accept()
-        clients.append(client_socket)
-        print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
-
-        threading.Thread(target=handle_client, args=(client_socket,)).start()
+        try:
+            client_socket, client_address = server.accept()
+            clients.append(client_socket)
+            print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+            executor.submit(handle_client, client_socket)
+        except KeyboardInterrupt:
+            print("\n Shutting down server...")
+            for client in clients:
+                client.close()
+            server.close()
 
 if __name__ == "__main__":
     start_server()
